1e6bb69e8b2fb779a83ca00d99da8c05948a514a
hasini-1 <balasrihasinikoppuravuri@gmail.com>
2026-08-02 20:13:43 +0530

    made responsive

--- script.js
+++ script.js
@@ -60,3 +60,10 @@
         }
       });
     });
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("hidden");
+});
